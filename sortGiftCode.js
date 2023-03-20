fdf2af37fce00d80012163bfe53ff3761b611762
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-03-20 10:48:10 -0400

    sorts gift code in alphabetical order

--- sortGiftCode.js
+++ sortGiftCode.js
@@ -12,9 +12,16 @@
 //   return reversedArr.join('');
 // }
 
-const sortedGiftCode = code => code.split('').sort().join('');
+// const sortedGiftCode = code => code.split('').sort().join('');
 
 //examples
 console.log(sortedGiftCode('abcdef'), 'abcdef');
 console.log(sortedGiftCode('pqksuvy'), 'kpqsuvy');
 console.log(sortedGiftCode('zyxwvutsrqponmlkjihgfedcba'), 'abcdefghijklmnopqrstuvwxyz');
+
+
+// Sort the Gift Code second time (7kyu)
+// accepts a string containing up to 26 unique alphabetical characters
+// return a string containing the same characters in alphabetical order
+
+const sortedGiftCode = code => code.split('').sort().join();
