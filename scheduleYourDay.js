57448472c76fc0a353b3e02a1d742ff3793f531c
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-04-29 15:21:44 -0400

    made code more concise

--- scheduleYourDay.js
+++ scheduleYourDay.js
@@ -11,24 +11,28 @@
   // It should also be in minutes, rounded to the nearest whole minute.
   // If your work is going to take more time than you have, return "You're not sleeping tonight!"
 
-function dayPlan (hours, tasks, duration){
-  // tally total time spent performing tasks
-  let timeWorking = (tasks * duration) / 60;
-  let schedule = [];
-  // check for first condition --> too much work
-  if (timeWorking > hours ) return 'You\'re not sleeping tonight!';
-  // total time - time spent performing tasks determines leisure time.
-  let breakTime = (hours - timeWorking) * 60;
-  let breaks = tasks -1;
-  let breakDuration = Math.round(breakTime / breaks);
-  // alternate schedule between tasks and breaks
-  while (breaks > 0) {
-    schedule.push(duration, breakDuration);
-    breaks--;
-  }
-  schedule.push(duration);
-  return schedule;
-}
+// function dayPlan (hours, tasks, duration){
+//   // tally total time spent performing tasks
+//   let timeWorking = (tasks * duration) / 60;
+//   let schedule = [];
+//   // check for first condition --> too much work
+//   if (timeWorking > hours ) return 'You\'re not sleeping tonight!';
+//   // total time - time spent performing tasks determines leisure time.
+//   let breakTime = (hours - timeWorking) * 60;
+//   let breaks = tasks -1;
+//   let breakDuration = Math.round(breakTime / breaks);
+//   // alternate schedule between tasks and breaks
+//   while (breaks > 0) {
+//     schedule.push(duration, breakDuration);
+//     breaks--;
+//   }
+//   schedule.push(duration);
+//   return schedule;
+// }
+
+const dayPlan = (hours, tasks, duration) =>
+  duration * tasks > hours * 60 ? `You're not sleeping tonight!` :
+    [...Array(2 * tasks - 1)].map((_, idx) => idx & 1 ? Math.round((hours * 60 - duration * tasks) / (tasks - 1)) : duration);
 
 //examples
 console.log(dayPlan(8, 5, 30), [ 30, 83, 30, 83, 30, 83, 30, 83, 30 ]);
