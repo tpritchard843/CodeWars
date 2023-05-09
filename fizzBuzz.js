6bbeb0922f3c7165696c9f59f8e920065d321bb6
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-05-09 14:40:22 -0400

    cleaned up code to reduce lines and data usage

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -4,19 +4,32 @@
 // Multiples of 5 should print Buzz
 // Multiples of 3 and 5 should print FizzBuzz
 
+// function fizzBuzz() {
+//   // loop to print numbers to the console
+//   for (let i= 1; i < 101; i++) {
+//     // check for FizzBuzz condition first
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('FizzBuzz');
+//     }
+//     // check for multiples of 3
+//     else if (i % 3 === 0) {
+//       console.log('Fizz');
+//     }
+//     // check for multiples of 5
+//     else if (i % 5 === 0) {
+//       console.log('Buzz');
+//     }
+//     else console.log(i);
+//   }
+// }
+
 function fizzBuzz() {
-  // loop to print numbers to the console
   for (let i= 1; i < 101; i++) {
-    // check for FizzBuzz condition first
     if (i % 3 === 0 && i % 5 === 0) {
       console.log('FizzBuzz');
-    }
-    // check for multiples of 3
-    else if (i % 3 === 0) {
+    }else if (i % 3 === 0) {
       console.log('Fizz');
-    }
-    // check for multiples of 5
-    else if (i % 5 === 0) {
+    }else if (i % 5 === 0) {
       console.log('Buzz');
     }
     else console.log(i);
