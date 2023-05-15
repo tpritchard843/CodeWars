7fab8e2ca94f03359e2f4bc330b025e525aa9c36
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-05-15 14:29:54 -0400

    moved grades list to within the function scope

--- sortClimbingGrades.js
+++ sortClimbingGrades.js
@@ -3,10 +3,9 @@
 // ou will be given a list (lst) of climbing grades and you have to write a function (sort_grades) to return a list of the grades sorted easiest to hardest.
 // If the input is an empty list, return an empty list; otherwise the input will always be a valid list of one or more grades.
 
-const grades = ["VB", "V0", "V0+", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13", "V14", "V15", "V16", "V17"];
-
 function sortGrades(list){
   // create variable containing grades in order
+  const grades = ["VB", "V0", "V0+", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12", "V13", "V14", "V15", "V16", "V17"];
   // sort the list using the indices of grades list
   return list.sort((a,b) => grades.indexOf(a) - grades.indexOf(b));
 }
