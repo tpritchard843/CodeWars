e2bb111550abb93245831c5aa1ffebf76db6eb18
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-07-06 12:57:48 -0400

    refactored for more readable code

--- oddEvenSort.js
+++ oddEvenSort.js
@@ -3,25 +3,32 @@
 // 0 is considered to be an even index
 // All input strings are valid with no spaces
 
-function sortMyString(str) {
-  // split string into an array
-  let evens = [];
-  let odds = [];
+// function sortMyString(str) {
+//   // split string into an array
+//   let evens = [];
+//   let odds = [];
 
-  let charactersArr = str.split('');
-  // loop through array and check each character for even index/0 or odd index
-  charactersArr.forEach( (char, i) => {
-    if (i % 2 === 0 || i === 0) {
-      // save evens/0 in one variable
-      evens.push(char);
-    } else {
-      // and odds in another variable
-      odds.push(char);
-    }
-  })
-  // rejoin the two with a space between themand return the string
-  return evens.join('') + ' ' + odds.join('');
+//   let charactersArr = str.split('');
+//   // loop through array and check each character for even index/0 or odd index
+//   charactersArr.forEach( (char, i) => {
+//     if (i % 2 === 0 || i === 0) {
+//       // save evens/0 in one variable
+//       evens.push(char);
+//     } else {
+//       // and odds in another variable
+//       odds.push(char);
+//     }
+//   })
+//   // rejoin the two with a space between themand return the string
+//   return evens.join('') + ' ' + odds.join('');
+// }
+
+const sortMyString = s => {
+  let even = s.split('').filter((v, i) => i % 2 === 0).join('');
+  let odd = s.split('').filter((v, i) => i % 2 !== 0).join('');
+  return even + ' ' + odd;
 }
+
 
 // examples
 console.log(sortMyString("CodeWars"), "CdWr oeas");
