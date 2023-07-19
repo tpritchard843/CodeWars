6288401634670bae94e782b2325b2beeffe6e5eb
unexpectedhaki <tpritchard843@gmail.com>
2023-07-19 15:08:50 -0400

    Solved transpose strings kata

--- cw-07-19-23/main.js
+++ cw-07-19-23/main.js
@@ -1,1 +1,14 @@
 // your code here
+// 7kyu transpose two strings in an array
+function transposeTwoStrings (arr) {
+    let result = []
+    let longestStr = Math.max(arr[0].length, arr[1].length)
+
+    for (let i = 0; i < longestStr; i++) {
+        let row = (arr[0][i] || ' ') + ' ' + (arr[1][i] || ' ')
+        result.push(row)
+    }
+    return result.join('\n')
+}
+
+console.log(transposeTwoStrings(['joey', 'louise']))
