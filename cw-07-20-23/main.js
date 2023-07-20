e3267bc451246ea7b34da105f4de854215092f14
unexpectedhaki <tpritchard843@gmail.com>
2023-07-20 11:13:09 -0400

    Refactored alphabet challenge

--- cw-07-20-23/main.js
+++ cw-07-20-23/main.js
@@ -7,6 +7,24 @@
     return arr.map( word => word.toLowerCase().split('').filter( (y, i) => y == alphabet[i]).length);
 }; */
 
+function solve(arr) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let result = [];
 
+    //first for loop runs through each word in our initial array
+    for (let i = 0; i < arr.length; i++) {
+        let count = 0; // count variable to keep track of letters in correct position for each word in the initial array
+        // loop through the characters of the word
+        for (let j = 0; j < arr[i].length; j++) {
+            // if character matches the correct position in the alphabet, increment the count
+            if (arr[i][j].toLowerCase() == alphabet[j]) {
+                count++;
+            }
+        }
+        //push the count of correct letters for each word to the result array
+        result.push(count);
+    }
+    return result;
+}
 
 console.log(solve(["abode","ABc","xyzD"]))
