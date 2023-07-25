979c27e8accf3c44be70225d03b71b33d8abdd60
thomaspritchard20 <thomaspritchard20@penguin>
2023-07-25 11:21:16 -0400

    tram capacity kata

--- cw-07-25-2023/main.js
+++ cw-07-25-2023/main.js
@@ -8,3 +8,20 @@ function shuffleIt(arr, ...swaps) {
 }
 
 console.log(shuffleIt([1,2,3,4,5],[1,2])) // returns [1,3,2,4,5] 
+
+// 7kyu Tram capacity
+function tram(stops, descending, onboarding){
+    let max = 0;
+    let passengers = 0;
+    for (let i =0; i < stops; i++) {
+        // track number of passengers at each stop
+        passengers += onboarding[i] - descending[i];
+        //if current max capacity is less than the updated passengers count, set that as the new max
+        if (max < passengers) {
+            max = passengers;
+        }
+    }
+    return max; // return max capacity
+}
+
+console.log(tram(4, [0, 2, 4, 4], [3, 5, 2, 0]));
