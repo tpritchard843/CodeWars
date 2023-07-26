a30eb9823edcf667c0a3853e51692be435892836
tpritchard843 <tpritchard843@gmail.com>
2023-07-26 14:18:55 -0400

    refactored Powers of 4

--- cw-07-26-2023/main.js
+++ cw-07-26-2023/main.js
@@ -2,9 +2,10 @@
 
 function powerOf4(n) {
   //true if power of four
-  //false if not power of four or not a number
-    if (typeof n === 'number' && n > 0) {
-        return (Math.log(n) / Math.log(4)) % 1 === 0;
-    } 
-    return false;
+  //false if not power of four or not a positive number
+    //if (typeof n === 'number' && n > 0) {
+        //return (Math.log(n) / Math.log(4)) % 1 === 0;
+    //} 
+    //return false;
+    return Number.isInteger(n) && Number.isInteger(Math.log(n) / Math.log(4));
 }
