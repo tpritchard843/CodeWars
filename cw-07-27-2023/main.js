51011f9d70383e11444b667cbdb9f43600c55431
unexpectedhaki <tpritchard843@gmail.com>
2023-07-27 12:04:27 -0400

    keepOrder kata

--- cw-07-27-2023/main.js
+++ cw-07-27-2023/main.js
@@ -8,5 +8,16 @@ String.prototype.toJadenCase = function() {
 };
 
 var str = "How can mirrors be real if our eyes aren't real";
-console.log(str.toJadenCase())
-//str.toJadenCase()
+//console.log(str.toJadenCase());
+
+
+
+// 7kyu Keep the order
+
+function keepOrder(arr, val) {
+    //return the index at which val can be inserted whilst maintaining the sorted order of the arr
+    return arr.filter(x => x < val).length;
+}
+
+console.log(keepOrder([1, 2, 3, 4, 7], 5)) // i = 4
+console.log(keepOrder([1, 2, 3, 4], -1)) // i = 0
