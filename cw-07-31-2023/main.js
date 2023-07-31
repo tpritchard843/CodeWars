b06b7287265e09b5dadcec4c1e63789cf51b9afb
unexpectedhaki <tpritchard843@gmail.com>
2023-07-31 11:32:49 -0400

    Solved maximum multiple

--- cw-07-31-2023/main.js
+++ cw-07-31-2023/main.js
@@ -3,6 +3,18 @@
 // return minimum time (intervals of 1) needed to cook n pancakes with m num of pancakes that can fit in the pan
 //each pancake needs 1 minute per side
 
-function cookPancakes(n,m) {
-    return n < m ? 2 : Math.ceil((n*2) / m);
+// function cookPancakes(n,m) {
+//     return n < m ? 2 : Math.ceil((n * 2) / m);
+// }
+
+const cookTime = (n,m) => n < m ? 2 : Math.ceil((n * 2) / m);
+
+
+// 7kyu Maximum Multiple
+
+function maxMultiple(divisor, bound){
+    // console.log(bound % divisor);
+    return bound - bound % divisor; //subtract remainder from bound to get our highest multiple
 }
+
+console.log(maxMultiple(37, 200)) // 185
