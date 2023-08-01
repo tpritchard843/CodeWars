a78b794b2e7510dc08f605b66b261cfdef725561
unexpectedhaki <tpritchard843@gmail.com>
2023-08-01 13:32:32 -0400

    Refactored needle 8kyu

--- cw-08-01-2023/main.js
+++ cw-08-01-2023/main.js
@@ -1,10 +1,6 @@
 // 8kyu needle in a haystack
 
-function findNeedle(haystack) {
-    if (haystack.some(elem => elem === 'needle')) {
-      return `found the needle at position ${haystack.indexOf('needle')}`
-    }
-    return "Your function didn't return anything"
-  }
+const findNeedle = haystack => `found the needle at position ${haystack.indexOf('needle')}`
 
-console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]))
+
+//console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]));
