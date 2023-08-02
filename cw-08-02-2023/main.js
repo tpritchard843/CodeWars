b7c2a68f52a4e4f3f47a06272ce0db44eb65bfbe
unexpectedhaki <tpritchard843@gmail.com>
2023-08-02 10:11:08 -0400

    Solved 1st 7kyu for array ladder

--- cw-08-02-2023/main.js
+++ cw-08-02-2023/main.js
@@ -4,3 +4,20 @@
 //Given an array of integers, return a new array with each value doubled
 
 const map = arr => arr.map(num => num * 2);
+
+
+// 7kyu sum of two lowest positive integers
+// given an array with at least 4 positive integers(no floats or non-positive integers), return the sum of the two lowest positive integers
+
+// function sumTwoSmallestNumbers(numbers) {
+//     const sortedArr = numbers.sort((a,b) => a-b);
+//     const lowest = sortedArr[0];
+//     const secondLowest = sortedArr[1];
+//     console.log(sortedArr, lowest, secondLowest);
+//     return lowest + secondLowest;
+// }
+
+const sumTwoSmallestNumbers = numbers => numbers.sort((a,b) => a-b).reduce((acc, c, i) => i < 2 ? acc + c : acc + 0);
+
+console.log(sumTwoSmallestNumbers([3, 1, 5, 6, 8]));
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
