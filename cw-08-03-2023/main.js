ff35044ee2400c76cfe7122daae99fb491d707fa
unexpectedhaki <tpritchard843@gmail.com>
2023-08-03 14:11:04 -0400

    Solved Parts of a list kata

--- cw-08-03-2023/main.js
+++ cw-08-03-2023/main.js
@@ -24,3 +24,22 @@ class Block{
         return 2 * (l*w + l*h + w*h);
   }
 }
+
+
+// 7kyu Parts of a list (arrays)
+// Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+// Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+// Each part will be in a string
+// Elements of a pair must be in the same order as in the original array.
+
+function partlist(arr) {
+    let newArr = [];
+    let results= [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr.slice(0,i).join(" ")); // first val 0 to i
+        newArr.push(arr.slice(i).join(" ")); // second val i onward
+        results.push(newArr);
+    }
+
+    return results;
+}
