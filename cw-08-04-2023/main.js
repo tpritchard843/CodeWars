92e7b34c4eedb9667f5d6a9ddcae40c66bb511ef
unexpectedhaki <tpritchard843@gmail.com>
2023-08-04 12:10:44 -0400

    Solved Likes kata

--- cw-08-04-2023/main.js
+++ cw-08-04-2023/main.js
@@ -16,3 +16,17 @@ function getCount(str) {
 }
 
 console.log(getCount("abracadabra"));
+
+
+// 6kyu Who Likes it?
+
+function likes(names) {
+  names = names || [];
+  switch(names.length) {
+    case 0: return 'no one likes this';
+    case 1: return `${names[0]} likes this`; 
+    case 2: return `${names[0]} and ${names[1]} like this`;
+    case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default: return `${names[0]}, ${names[1]}, and ${names.length - 2} others like this`
+  }
+}
