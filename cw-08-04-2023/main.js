d9e2ccdde1bc2f90495389f86de9fefbb4feb62d
unexpectedhaki <tpritchard843@gmail.com>
2023-08-04 11:53:44 -0400

    Solved vowel count kata

--- cw-08-04-2023/main.js
+++ cw-08-04-2023/main.js
@@ -6,3 +6,19 @@ function numberToString(num) {
     return String(num);
     //return num.toString();
 }
+
+
+// 7kyu Vowel Count
+
+function getCount(str) {
+    let count = 0;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(getCount("abracadabra"));
