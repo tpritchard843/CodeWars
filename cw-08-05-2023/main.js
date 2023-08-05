2c2859108db53705ddcf01c142ca315d8d77c790
unexpectedhaki <tpritchard843@gmail.com>
2023-08-05 19:53:32 -0400

    Refactored middle char function using str.slice()

--- cw-08-05-2023/main.js
+++ cw-08-05-2023/main.js
@@ -1,13 +1,7 @@
+//7kyu Get the middle character
+
 function getMiddle(str) {
-    let lettersArr = str.split('');
-    console.log(lettersArr);
-  if (str.length % 2 === 0) {
-    let firstHalf = lettersArr.slice(0, Math.ceil(lettersArr.length / 2));
-    let secondHalf = lettersArr.slice(Math.ceil(lettersArr.length / 2));
-    return firstHalf[firstHalf.length - 1] + secondHalf[0];
-  } else {
-    return lettersArr[(lettersArr.length - 1) / 2]
-  }
+    return str.slice((str.length -1) / 2, str.length / 2 + 1);
 }
 
 console.log(getMiddle('testing'))
