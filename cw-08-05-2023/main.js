5c1ab952772de2c168f922313d0c2f41335dfbaf
unexpectedhaki <tpritchard843@gmail.com>
2023-08-05 20:19:32 -0400

    typo fixes

--- cw-08-05-2023/main.js
+++ cw-08-05-2023/main.js
@@ -4,8 +4,8 @@ function getMiddle(str) {
     return str.slice((str.length -1) / 2, str.length / 2 + 1);
 }
 
-console.log(getMiddle('testing'))
-console.log(getMiddle('test'))
+console.log(getMiddle('testing'));
+console.log(getMiddle('test'));
 
 
 
