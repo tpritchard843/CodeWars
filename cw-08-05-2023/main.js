6b872119e9d8cd45393584c0da74634e8f700bbb
unexpectedhaki <tpritchard843@gmail.com>
2023-08-05 20:19:14 -0400

    Added notes

--- cw-08-05-2023/main.js
+++ cw-08-05-2023/main.js
@@ -12,5 +12,6 @@ console.log(getMiddle('test'))
 // 6kyu spin words 
 
 const spinWords = string => string.split(" ").map(word => word.length < 5 ? word : [...word].reverse().join("")).join(" ");
+//spread operator spreads each letter into an array, allowing us to reverse the word. Works similarly to .split() in this context
 
 console.log(spinWords("Hey fellow warriors"));
