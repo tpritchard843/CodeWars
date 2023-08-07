7c89bd0f640323c55413aff34a2dcc5bd4102932
unexpectedhaki <tpritchard843@gmail.com>
2023-08-07 10:49:03 -0400

    Solved find equal index kata

--- cw-08-07-2023/main.js
+++ cw-08-07-2023/main.js
@@ -4,3 +4,24 @@ function minMax(arr){
 }
 
 console.log(minMax([1,2,3,4,5]));
+
+
+// 6kyu Equal Sides of an Array
+
+function findEvenIndex(arr) {
+    let left = 0; 
+    let right = arr.reduce((acc, c) => acc + c, 0);
+
+    for (let i = 0; i < arr.length; i++) {
+        right -= arr[i];
+        if (left == right) return i;
+        left += arr[i];
+    }
+    return -1;
+}
+
+console.log(findEvenIndex([1,2,3,4,3,2,1])); // index 3
+//right 15, != 0 --> left + 1
+//right 13, != 1 --> left + 2 = 3
+//right 10, != 3 --> left + 3 = 6
+// right 6 == left 6 --> return index 3
