4fd1653862ecf3ab35c51ecb305e0cc2cef7688a
unexpectedhaki <tpritchard843@gmail.com>
2023-08-22 08:54:40 -0400

    Refactored to remove unnecessary conditional

--- anagrams.js
+++ anagrams.js
@@ -3,17 +3,10 @@
 // Returns true or false
 
 let isAnagram = function(test, original) {
-  // check if length of two words matches. If no match, return false
-  if (test.length !== original.length) return false;
   // sort each word alphabetically and check for equality between the two
   let str1 = test.toLowerCase().split('').sort().join('');
   let str2 = original.toLowerCase().split('').sort().join('');
-  console.log(str1, str2);
-  if (str1 === str2) {
-    return true;
-  } else {
-    return false;
-  }
+  return str1 === str2 ? true : false;
 };
 
 //Examples
