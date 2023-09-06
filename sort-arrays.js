3b86cd2560ef48e5b04b732079adb6e88ea3a2e6
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-09-06 10:24:25 -0400

    sorts an arr of strings in lexicographical order

---
+++ sort-arrays.js
@@ -0,0 +1,9 @@
+// 7kyu Sort Arrays 1
+// takes in an array as a parameter and asks us to return the array sorted in lexicographical order
+
+// loop through each itme of the array and sort them in alphabetical order
+const sortme = names => names.sort();
+
+
+// Examples
+console.log(sortme(['one', 'two', 'three' ]));
