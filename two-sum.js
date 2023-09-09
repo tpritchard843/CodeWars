98462b85e96ed12adf2ce29070f4c461078a1416
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-09-08 23:49:30 -0400

    Refactored using a hash map for o(n)

--- two-sum.js
+++ two-sum.js
@@ -18,6 +18,21 @@
 
 // There are alot of comments of this solution being O(n^2). That means that for every item in the array you are going through every other item in the array. There is a solution that is O(n). Which means you go through each item in the array a constant number of times but you still view each item in the array at least once. It itterates through once and stores the index of the number in a hash. Then itterates through again and uses the hash to find the index of the pair that adds to the target. This is an example of trading space for time complexity. See below
 
+// function twoSum(nums, target) {
+//   let hash = {};
+//   nums.forEach( (num, i) => {
+//     hash[num] = i;
+//   })
+//   for (let j=0; j<nums.length; j++) {
+//     let pair = target - nums[j];
+//     let i = hash[pair];
+//     if (i != undefined && i != j) {
+//       return [j, i];
+//     }
+//   }
+//   return [0,0];
+// }
+
 function twoSum(nums, target) {
   let hash = {};
   nums.forEach( (num, i) => {
