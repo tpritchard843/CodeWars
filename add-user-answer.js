3880915bf01f86ea8bf8f8943eb2dcd8b93df710
unexpectedhaki <tpritchard843@gmail.com>
2023-09-13 16:20:51 -0400

    adds a userAnswers prop to each obj in arr

---
+++ add-user-answer.js
@@ -0,0 +1,22 @@
+// 7kyu Add userAnswer to each object in array
+// add new property userAnswer to every object in an array and set value to null
+
+var questions = [{
+  question: "What's the currency of the USA?",
+  choices: ["US dollar", "Ruble", "Horses", "Gold"],
+  corAnswer: 0
+}, {
+  question: "Where was the American Declaration of Independence signed?",
+  choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+  corAnswer: 0
+}];
+
+function addUserAnswer(questions) {
+  questions.forEach(obj => {
+    obj.usersAnswer = null;
+  })
+  return questions;
+}
+
+//addUserAnswer(questions);
+console.log(addUserAnswer(questions));
