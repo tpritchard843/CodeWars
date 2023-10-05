bd6b486aa05fc084c5adceace30660ee00f6d72a
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-10-05 15:27:26 -0400

    returns a list of friends

---
+++ friendOrFoe.js
@@ -0,0 +1,15 @@
+// Friend or Foe 7kyu
+
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+function friend(friends){
+  // loop through list
+  // filter by names with length === 4
+  return friends.filter(name => name.length === 4);
+}
+
+console.log(friend(["Ryan", "Kieran", "Mark"]), ["Ryan", "Mark"]);
+console.log(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"]);
+console.log(friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"]), ["Jimm", "Cari", "aret"]);
+console.log(friend(["Love", "Your", "Face", "1"]), ["Love", "Your", "Face"]);
