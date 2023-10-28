25e5e6f6f2c6f9e8a5652a348084e0fd51853be3
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-10-28 17:06:58 -0400

    returns a sorted code

---
+++ sortGiftCode.js
@@ -0,0 +1,20 @@
+// Sort the Gift Code second time (7kyu)
+// accepts a string containing up to 26 unique alphabetical characters
+// return a string containing the same characters in alphabetical order
+
+
+// function sortGiftCode(code){
+//   // split code str into an array
+//   let charactersArr = code.split('');
+//   // sort the array
+//   let reversedArr = charactersArr.sort();
+//   // join the array back together as a string and return
+//   return reversedArr.join('');
+// }
+
+const sortedGiftCode = code => code.split('').sort().join('');
+
+//examples
+console.log(sortedGiftCode('abcdef'), 'abcdef');
+console.log(sortedGiftCode('pqksuvy'), 'kpqsuvy');
+console.log(sortedGiftCode('zyxwvutsrqponmlkjihgfedcba'), 'abcdefghijklmnopqrstuvwxyz');
