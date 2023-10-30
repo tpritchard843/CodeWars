8ac1ad90e08224cf8c0473570130b5a727f74998
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-10-30 11:23:01 -0400

    adds nums to sum & subtracts string ints from sum

---
+++ divideConquer.js
@@ -0,0 +1,16 @@
+// Divide and conquer 7kyu
+
+// Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+// no funny biz, array will contain only nums and string nums
+// return as a number
+
+
+function divCon(arr){
+
+}
+
+
+// examples
+console.log(divCon([9, 3, '7', '3']), 2);
+console.log(divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 14);
+console.log(divCon(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 13);
