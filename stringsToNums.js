28d28b61e3b9231a03499d875eb737d99449f046
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-11-04 12:50:08 -0400

    converted to one line using map method

--- stringsToNums.js
+++ stringsToNums.js
@@ -1,16 +1,17 @@
 // Convert an array of strings to an array of numbers (7kyu)
 // you can receive floats as well
 
-function toNumberArray(stringArr){
-  let numbersArr = [];
-  // loop through string arr
-    // for each string in stringArr, convert to Num using Number() constructor
-  stringArr.forEach(str => {
-    let num = Number(str);
-    numbersArr.push(num);
-  })
-  return numbersArr;
-}
+// function toNumberArray(stringArr){
+//   let numbersArr = [];
+//   // loop through string arr
+//     // for each string in stringArr, convert to Num using Number() constructor
+//   stringArr.forEach(str => {
+//     let num = Number(str);
+//     numbersArr.push(num);
+//   })
+//   return numbersArr;
+// }
 
+const toNumberArray = stringArr => stringArr.map(str => Number(str));
 // Examples
 console.log(toNumberArray(["1.1","2.2","3.3"]), [1.1,2.2,3.3])
