9527c2ed2a84bcdacd81e9ad5fa78da93e96dea1
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-11-06 11:48:01 -0500

    converts strings to nums

---
+++ stringsToNums.js
@@ -0,0 +1,16 @@
+// Convert an array of strings to an array of numbers (7kyu)
+// you can receive floats as well
+
+function toNumberArray(stringArr){
+  let numbersArr = [];
+  // loop through string arr
+    // for each string in stringArr, convert to Num using Number() constructor
+  stringArr.forEach(str => {
+    let num = Number(str);
+    numbersArr.push(num);
+  })
+  return numbersArr;
+}
+
+// Examples
+console.log(toNumberArray(["1.1","2.2","3.3"]), [1.1,2.2,3.3])
