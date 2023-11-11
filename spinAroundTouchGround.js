feeb8a1c5583b3d4e63bb4782705da5c89c9cd03
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-11-11 10:45:36 -0500

    returns num of spins based on list of directions

---
+++ spinAroundTouchGround.js
@@ -0,0 +1,6 @@
+// Spin Around, Touch the Ground (7kyu)
+// Given a list of directions to spin, "left" or "right", return an integer of how many full 360° rotations were made. Note that each word in the array counts as a 90° rotation in that direction.
+
+function spinAround(turns) {
+  return Math.floor(Math.abs(turns.reduce((sum, c) => c === 'right' ? sum + 90 : sum - 90, 0) / 360));
+}
