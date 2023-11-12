d85a15986cf0a868f6b3c554e7420471e5fe4083
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-11-12 10:47:01 -0500

    converted to arrow function to store value

--- spinAroundTouchGround.js
+++ spinAroundTouchGround.js
@@ -1,6 +1,8 @@
 // Spin Around, Touch the Ground (7kyu)
 // Given a list of directions to spin, "left" or "right", return an integer of how many full 360° rotations were made. Note that each word in the array counts as a 90° rotation in that direction.
 
-function spinAround(turns) {
-  return Math.floor(Math.abs(turns.reduce((sum, c) => c === 'right' ? sum + 90 : sum - 90, 0) / 360));
-}
+// function spinAround(turns) {
+//   return Math.floor(Math.abs(turns.reduce((sum, c) => c === 'right' ? sum + 90 : sum - 90, 0) / 360));
+// }
+
+const spins = turns => Math.floor(Math.abs(turns.reduce((sum, c) => c === 'right' ? sum + 90 : sum - 90, 0) / 360));
