e4d8660a485112f855eae28d94f560d1e14e8bfa
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-11-18 15:58:25 -0500

    returns longest number

---
+++ mostDigits.js
@@ -0,0 +1,16 @@
+// 7kyu Most Digits
+// Find the number with the most digits.
+// If two numbers in the argument array have the same number of digits, return the first one in the array.\
+// can't change order of the array
+
+function findLongest(array){
+  // loop through the array
+  // convert each number to a String
+  // check for length and return first instance, converted back to a number
+  return array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a));
+}
+
+// examples
+console.log(findLongest([1, 10, 100]), 100);
+console.log(findLongest([9000, 8, 800]), 9000);
+console.log(findLongest([8, 900, 500]), 900);
