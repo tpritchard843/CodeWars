632afda1603b4a17176d872b8400c2a256aaad36
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-11-20 15:59:32 -0500

    converted to ES6 arrow func stored as a variable

--- mostDigits.js
+++ mostDigits.js
@@ -3,12 +3,14 @@
 // If two numbers in the argument array have the same number of digits, return the first one in the array.\
 // can't change order of the array
 
-function findLongest(array){
-  // loop through the array
-  // convert each number to a String
-  // check for length and return first instance, converted back to a number
-  return array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a));
-}
+// function findLongest(array){
+//   // loop through the array
+//   // convert each number to a String
+//   // check for length and return first instance, converted back to a number
+//   return array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a));
+// }
+
+const longestNumber = arr => arr.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a));
 
 // examples
 console.log(findLongest([1, 10, 100]), 100);
