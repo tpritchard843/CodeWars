318d97a8b6561dd03167db07721356f43c268862
unexpectedhaki <thomas.pritchard20@gmail.com>
2023-12-11 08:29:15 -0500

    solved roadbumps again

--- roadBumps.js
+++ roadBumps.js
@@ -3,7 +3,7 @@
 // Given a string showing either flat road (_) or bumps (n)
 // If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
 
-const bump = str => str.split('n').length > 16 ? 'Car Dead' : 'Woohoo!';
+// const bump = str => str.split('n').length > 16 ? 'Car Dead' : 'Woohoo!';
 
 //examples
 console.log(bump("n"), "Woohoo!");
@@ -11,3 +11,6 @@ console.log(bump("__nn_nnnn__n_n___n____nn__nnn"), "Woohoo!");
 console.log(bump("nnn_n__n_n___nnnnn___n__nnn__"), "Woohoo!");
 console.log(bump("_nnnnnnn_n__n______nn__nn_nnn"), "Car Dead");
 console.log(bump("______n___n_"), "Woohoo!");
+
+
+const bump = str => str.split('n').length > 16 ? 'Car Dead' : 'Woohoo!';
